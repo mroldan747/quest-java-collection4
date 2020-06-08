d9834bd88625eea0cf5ab05adaf3eeeae2b08703
mroldan747 <mroldan747@gmail.com>
2020-06-08 09:38:22 +0200

    Map party done

--- Thanos.java
+++ Thanos.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -16,15 +18,35 @@ public class Thanos {
         Flower violet = new Flower("Violet");
         Flower rose = new Flower("Rose");
 
-        // TODO 1 : Create a TreeMap where :
+        Comparator<Hero> comparator = new Comparator<Hero>() {  
+            @Override  
+            public int compare(Hero orig, Hero compare) {  
+                return orig.getName().compareTo(compare.getName());  
+            }  
+        }; 
+        Map<Hero,Flower> party = new TreeMap<>(comparator);
         // Hulk, Thor and Scarlet Witch have a Rose
+        party.put(hulk, rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
+
         // Vision has a Tulip
+        party.put(vision, tulip);
+    
         // Captain America has a Lily
+        party.put(captainAmerica, lily);
+
         // Doctor Strange and Black Widow have a Violet
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
 
-        // TODO 2 : Print if `begonia` is contained in the TreeMap
 
-        // TODO 3 : For each hero, alphabetically, print the corresponding flower
+        System.out.println(party.containsValue(begonia)); 
 
+
+        for (Hero hero : party.keySet()) {
+            Flower flower = party.get(hero);
+            System.out.println(flower.getName());
+        }
     }
 }
